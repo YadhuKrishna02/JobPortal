d512122e94b387f3f5aded88ec3ee2999fd21f58
YadhuKrishna02 <i_am_yk02@protonmail.com>
2023-07-15 12:07:35 +0530

    tried to use effect bug fix 2nd time

--- frontend/src/pages/job-seeker/AppliedJobsList.jsx
+++ frontend/src/pages/job-seeker/AppliedJobsList.jsx
@@ -12,7 +12,7 @@ const JobList = () => {
 
   const [applying, setApplying] = useState(false); // Loading state for job application
 
-  const applicantId = useSelector((state) => state?.users?.profile?._id);
+  const applicantId = useSelector((state) => state?.users?.users?.applicantId);
   const dispatch = useDispatch();
 
   useEffect(() => {
